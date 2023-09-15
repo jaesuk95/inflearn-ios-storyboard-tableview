f72ad45f0469fc2e5f45632d5e054113f727c7cc
Jaesuk <jaesuk95@outlook.com>
2023-09-16 01:40:28 +0900

    tableView

---
+++ TableView/Models/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  TableView
+//
+//  Created by Jaesuk Lee on 2023/09/16.
+//
+
+import UIKit
+
+
+struct Movie {
+    let movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+    
+}
